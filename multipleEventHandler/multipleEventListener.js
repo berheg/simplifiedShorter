1e4f235a9d84c7d9a82a04b502244f7a3cff34f0
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-06-15 22:17:25 +0200

    function name modified

--- multipleEventHandler/multipleEventListener.js
+++ multipleEventHandler/multipleEventListener.js
@@ -1,7 +1,7 @@
 const theParent = document.querySelector('#eventHandlerDiv');
 theParent.addEventListener('click', diplayString,false);
 
-function diplayString(e){
+function displayString(e){
     if(e.target !== e.currentTarget){
         const clickedItem = e.target.id;
         alert("Hello " + clickedItem);
